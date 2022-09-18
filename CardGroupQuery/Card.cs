186d9b09425aa304dcda052aa547cbe2259502ff
osajab10@gmail.com <osajab10@gmail.com>
2022-09-18 18:36:50 +0200

    Add project files.

---
+++ CardGroupQuery/Card.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGroupQuery
+{
+    class Card : IComparable<Card>
+    {
+        public int CompareTo(Card other)
+        {
+            return new CardComparerByValue().Compare(this, other);
+        }
+
+        public Values Value { get; private set; }
+        public Suits Suit { get; private set; }
+
+        public Card(Values value, Suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+
+        public string Name
+        {
+            get { return $"{Value} of {Suit}"; }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
